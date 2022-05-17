11d83aee8ec8e7ef6912e705ecc4627737a97bfe
Octavio Llanillo <sirshanisho@gmail.com>
2022-05-17 15:24:17 -0300

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿    
+using System.Runtime.InteropServices;
+
+int Numero;
+
+Console.WriteLine("Ingrese un número");
+
+Numero = Convert.ToInt32(Console.ReadLine());
+
+if (Numero > 0)
+{
+    Console.WriteLine("El número invertido es: " + Invertir(Numero));
+}
+else
+{
+    Console.WriteLine("No cumple la condición para invertirlo");
+}
+
+int Invertir(int Numero)
+{
+    int Suma = 0;
+
+    while (Numero != 0)
+    {
+        Suma = Suma * 10 + Numero % 10;
+        Numero /= 10;
+    }
+
+    return Suma;
+}
+    
