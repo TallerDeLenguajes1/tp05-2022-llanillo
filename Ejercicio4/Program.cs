11a7e5cc34b93aa2720b0838072783410bac3dd3
Octavio Llanillo <sirshanisho@gmail.com>
2022-05-17 17:33:38 -0300

    Ejercicio5 Finalizado

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Text.RegularExpressions;
+
+string Cadena;
+string Cadena2;
+
+Console.WriteLine("Ingrese una cadena cualquiera");
+Cadena = Console.ReadLine();
+
+Console.WriteLine("Carácteres: " + Cadena[0] + " " + Cadena[1]);
+Console.WriteLine("Longitud: " + Cadena.Length);
+Console.WriteLine("Concatenada con 'pepe': " + String.Concat(Cadena, "pepe"));
+Console.WriteLine("Subcadena: " + Cadena.Substring(3));
+
+Console.WriteLine("Palabra mostrada carácter por carácter");
+foreach(char Caracter in Cadena){
+    Console.WriteLine(Caracter);
+}
+
+Console.WriteLine("Buscar la palabra 'pepe' en la cadena");
+Console.WriteLine(Cadena.Contains("pepe"));
+
+Console.WriteLine("Palabra en mayúsculas");
+Console.WriteLine(Cadena.ToUpper());
+
+Console.WriteLine("Palabra en minúsculas");
+Console.WriteLine(Cadena.ToLower());
+
+Console.WriteLine("Ingrese otra cadena");;
+Cadena2 = Console.ReadLine();
+
+Console.WriteLine("Comparación de las cadenas");
+Console.WriteLine();
+Console.WriteLine(String.Compare(Cadena, Cadena2));
+
+Console.WriteLine("Ingrese una ecuación");
+Cadena = Console.ReadLine();
+
+float Respuesta = (float) Convert.ToDouble(new DataTable().Compute(Cadena, null));
+Console.WriteLine("Respueata: " + Respuesta);
---
+++ README.md
@@ -0,0 +1,52 @@
+﻿### Ejercicio 4
+
+#### ¿String es una tipo por valor o un tipo por referencia?
+Por referencia
+#### ¿Qué secuencias de escape tiene el tipo string?
+\a	Bell (alert)
+
+\b	Backspace
+
+\f	Form feed
+
+\n	New line
+
+\r	Carriage return
+
+\t	Horizontal tab
+
+\v	Vertical tab
+
+\'	Single quotation mark
+
+\"	Double quotation mark
+
+\\	Backslash
+
+\?	Literal question mark
+
+\ ooo	ASCII character in octal notation
+
+\x hh	ASCII character in hexadecimal notation
+
+\x hhhh	Unicode character in hexadecimal notation if this escape sequence is used in a wide-character constant or a Unicode string literal.
+
+
+#### ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+
+El @ de prefijo significa que la cadena no usará secuencias de escape.
+El $ de prefijo significa que la cadena usara cadenas interpoladas
+
+### Ejercicio 5
+
+##### ¿Funcionan únicamente en C#?
+
+No
+
+#####  ¿En qué casos le parecen útiles? Enuncie al menos 3.
+
+Para controlar números, correos electrónicos, o cualquier expresión
+
+##### ¿Cómo se hace uso de estas en C#?
+
+Con la clase que ya trae C#
